9bcf2b703c101ec269b879e8c7fd7595a4db0c72
SakshiSrivastava55 <53784511+SakshiSrivastava55@users.noreply.github.com>
2020-11-26 12:36:32 +0530

    Update code.java

--- LAB_8/code.java
+++ LAB_8/code.java
@@ -1,106 +1,61 @@
 import java.util.*;
 
-public class Crc
+public class CRC{
 
-{
+    public static int n;
 
-            public static int n;
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        CRC ob=new CRC();
 
-            public static void main(String[] args)
+        String code, copy, rec,zero="0000000000000000";
 
-{
+        System.out.print("Enter poly: ");
+        code=in.nextLine();
 
-                        Scanner in=new Scanner(System.in);
+        n=code.length();
+        copy=code;
+        code+=zero;
 
-                        Crc ob=new Crc();
+        System.out.println("Modified poly: "+code);
 
-                        String code, copy, rec,zero="0000000000000000";
+        code=ob.divide(code);
 
-                        System.out.println("Enter message");
+        System.out.println("Remainder: "+code.substring(n));
 
-                        code=in.nextLine();
+        copy=copy.substring(0,n)+code.substring(n);
 
-                        n=code.length();
+        System.out.println("transmitted frame: "+copy);
 
-                        copy=code;
+        System.out.print("\nEnter recived data: ");
+        rec=in.nextLine();
 
-                        code+=zero;
+        if(zero.equals(ob.divide(rec).substring(n)))
+            System.out.println("Correct bits recieved");
+        else
+            System.out.println("Recieved frame contains one or more errors");
+    }  
 
-                        code=ob.divide(code);
+    public String divide(String s){
+        int i,j;
+        char x;
+        String div="10001000000100001";
 
-                        System.out.println("Message="+copy);
+        System.out.println("CRC = "+div);
 
-                        copy=copy.substring(0,n)+code.substring(n);
-
-                        System.out.println("CRC=");
-
-                        System.out.println(code.substring(n));
-
-                        System.out.println("transmitted frame is "+copy);
-
-                        System.out.println("Enter recived data");
-
-                        rec=in.nextLine();
-
-                        if(zero.equals(ob.divide(rec).substring(n)))
-
-                                    System.out.println("Correct bits recieved");
-
-                        else
-
-                                    System.out.println("Recieved frame contains one or more errors");
-
-                        in.close();
-
+        for(i=0;i<n;i++){
+            x=s.charAt(i);
+            
+            for(j=0;j<17;j++){
+                if(x=='1'){
+                    if(s.charAt(i+j)!=div.charAt(j))
+                        s=s.substring(0,i+j)+"1"+s.substring(i+j+1);
+                    else
+                        s=s.substring(0,i+j)+"0"+s.substring(i+j+1);
+                }
             }
+        }
 
-           
-
-           
-
-            public String divide(String s)
-
-{
-
-                                    int i,j;
-
-                                    char x;
-
-                                    String div="10001000000100001";
-
-                                    for(i=0;i<n;i++)
-
-                                    {
-
-                                                x=s.charAt(i);
-
-                                                for(j=0;j<17;j++)
-
-                                                {
-
-                                                            if(x=='1')
-
-                                                            {if(s.charAt(i+j)!=div.charAt(j))
-
-                                                                        s=s.substring(0,i+j)+"1"+s.substring(i+j+1);
-
-                                                            else
-
-                                                                        s=s.substring(0,i+j)+"0"+s.substring(i+j+1);
-
-                                                            }
-
-                                                           
-
-                                                }
-
-                                    }
-
-
-
-
-                                    return s;
-
-            }
-
+        return s;
+    }
 }
